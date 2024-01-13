9715facef861471606b646b45ebb51892e71322d
Bram Borggreve <beeman@beeman.nl>
2024-01-13 20:16:25 +0000

    chore: update PubKey UI

--- libs/web/shell/feature/src/lib/shell-layout.tsx
+++ libs/web/shell/feature/src/lib/shell-layout.tsx
@@ -1,4 +1,5 @@
 import { Group } from '@mantine/core'
+import { useDisclosure } from '@mantine/hooks'
 import {
   SolanaUiAccountBalanceButton,
   SolanaUiAccountChecker,
@@ -11,10 +12,13 @@ import { UiHeader, UiLayout, UiLoader } from '@pubkey-ui/core'
 import { ReactNode, Suspense } from 'react'
 
 export function ShellLayout({ children }: { children: ReactNode }) {
+  const [opened, { toggle }] = useDisclosure(false)
   return (
     <UiLayout
       header={
         <UiHeader
+          opened={opened}
+          toggle={toggle}
           links={[
             { link: '/dashboard', label: 'Dashboard' },
             { link: '/solana', label: 'Solana' },
