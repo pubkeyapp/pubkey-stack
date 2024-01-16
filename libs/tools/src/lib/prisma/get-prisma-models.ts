448fe8d2d35a4f1e47cc73c025b85fa5d29425f6
Bram Borggreve <beeman@beeman.nl>
2024-01-16 07:42:24 +0000

    feat: add prisma-sync generator

--- libs/tools/src/lib/prisma/get-prisma-models.ts
+++ libs/tools/src/lib/prisma/get-prisma-models.ts
@@ -4,5 +4,6 @@ import { getPrismaSchema } from './get-prisma-schema'
 
 export function getPrismaModels(tree: Tree, schemaPath = 'prisma/schema.prisma'): Model[] {
   const schema = getPrismaSchema(tree, schemaPath)
+
   return schema.list.filter((item) => item.type === 'model') as Model[]
 }
