b284a6443b43e9afda5fa4fd27e724e97793a504
Bram Borggreve <beeman@beeman.nl>
2024-01-21 03:21:22 +0000

    test: add snapshot test to web-feature generator

--- libs/tools/src/generators/web-feature/web-feature-generator.spec.ts
+++ libs/tools/src/generators/web-feature/web-feature-generator.spec.ts
@@ -1,22 +1,31 @@
 import { Tree } from '@nx/devkit'
 import { createTreeWithEmptyWorkspace } from '@nx/devkit/testing'
+import { getRecursiveFileContents } from '../../lib/utils/get-recursive-file-contents'
+import { normalizeWebFeatureSchema } from '../../lib/web'
 import { createMockWebApp } from '../../lib/web/create-mock-web-app'
 
 import { webFeatureGenerator } from './web-feature-generator'
-import { WebFeatureGeneratorSchema } from './web-feature-schema'
+import { type NormalizedWebFeatureSchema, WebFeatureGeneratorSchema } from './web-feature-schema'
 
 describe('web-feature generator', () => {
   let tree: Tree
   const rawOptions: WebFeatureGeneratorSchema = { app: 'web', name: 'test' }
+  let options: NormalizedWebFeatureSchema
 
   beforeEach(() => {
     tree = createTreeWithEmptyWorkspace()
+    options = normalizeWebFeatureSchema(tree, rawOptions)
   })
 
   it('should run successfully', async () => {
-    await createMockWebApp(tree, rawOptions.app)
-    await webFeatureGenerator(tree, rawOptions)
+    await createMockWebApp(tree, options.app)
+    await webFeatureGenerator(tree, options)
 
-    expect(true).toBeTruthy()
+    const contents = getRecursiveFileContents({
+      tree,
+      path: '.',
+      exclude: ['./package.json'],
+    })
+    expect(contents).toMatchSnapshot()
   })
 })
